7fcb58de38d797121ead2e9c412d45c6c5563f75
Prem Shewale <premshewale47@gmail.com>
2026-01-12 10:45:58 +0530

    Added Web http Methods

---
+++ Part16-Spring Web PUT and DELETE/src/main/java/com/telusko/Part16SimpleWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part16SimpleWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Part16SimpleWebAppApplication.class, args);
+    }
+
+}
---
+++ Part16-Spring Web PUT and DELETE/src/main/java/com/telusko/service/ProductService.java
@@ -0,0 +1,46 @@
+package com.telusko.service;
+
+import com.telusko.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private List<Product> products = new ArrayList<>(Arrays.asList(
+        new Product(101, "Iphone", 50000),
+        new Product(102, "Canon Camera", 70000),
+        new Product(103, "Shure Mic", 10000)
+    ));
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public Product getProductById(int prodId) {
+        return products.stream()
+            .filter(p -> p.getProdId() == prodId)
+            .findFirst()
+            .orElse(new Product(100, "No Item", 0));
+    }
+
+    public void addProduct(Product prod) {
+        products.add(prod);
+    }
+
+    public void updateProduct(Product prod) {
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getProdId() == prod.getProdId()) {
+                products.set(i, prod);
+                return;
+            }
+        }
+    }
+
+    public void deleteProduct(int prodId) {
+        products.removeIf(p -> p.getProdId() == prodId);
+    }
+}
