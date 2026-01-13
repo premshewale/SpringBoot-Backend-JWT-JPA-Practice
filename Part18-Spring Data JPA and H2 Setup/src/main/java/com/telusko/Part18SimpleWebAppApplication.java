a0be6d45bb884697d25de5e7d0bda43e16437c08
Prem Shewale <premshewale47@gmail.com>
2026-01-13 10:16:46 +0530

    Spring Data JPA

---
+++ Part18-Spring Data JPA and H2 Setup/src/main/java/com/telusko/Part18SimpleWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part18SimpleWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Part18SimpleWebAppApplication.class, args);
+    }
+
+}
