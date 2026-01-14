daf2e617276d382924f3a77d25a5a62eba34f335
Prem Shewale <premshewale47@gmail.com>
2026-01-14 11:00:11 +0530

    Spring Data Jpa with JpaRepo

---
+++ Part19-Spring Data JPA with JpaRepository/src/main/java/com/telusko/Part19SimpleWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part19SimpleWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Part19SimpleWebAppApplication.class, args);
+    }
+
+}
