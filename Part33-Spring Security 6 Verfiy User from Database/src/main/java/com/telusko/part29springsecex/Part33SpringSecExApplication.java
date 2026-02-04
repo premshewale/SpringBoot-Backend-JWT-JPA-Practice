b773f1195deb335825642f5095676065b7bc433d
Prem Shewale <premshewale47@gmail.com>
2026-02-04 11:41:12 +0530

    Spring Security Verify User From Database

---
+++ Part33-Spring Security 6 Verfiy User from Database/src/main/java/com/telusko/part29springsecex/Part33SpringSecExApplication.java
@@ -0,0 +1,13 @@
+package com.telusko.part29springsecex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part33SpringSecExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Part33SpringSecExApplication.class, args);
+    }
+
+}
